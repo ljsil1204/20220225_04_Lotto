36c5fc8fb1383ff5eaea1db80f71e13896cd563d
이진실 <ljsil1204@naver.com>
2022-02-25 14:37:34 +0900

    당첨번호를 뽑기 위한 반복
    - 1~45의 랜덤 값 추출
    - 중복 검사를 통과해야 다음 숫자를 뽑으러 넘어갈 예정

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -10,8 +10,8 @@ public class MainDrive {
 		Scanner myScanner = new Scanner(System.in);
 
 		int[] myNumbers = new int[6];
-		
-		for (int i = 0; i < 6; i++) {
+
+		for (int i = 0; i < myNumbers.length; i++) {
 
 			while (true) {
 				System.out.print((i + 1) + "번째 숫자 : ");
@@ -20,17 +20,17 @@ public class MainDrive {
 				boolean isRangeOk = (1 <= inputNum) && (inputNum <= 45);
 
 				boolean isDuplOk = true;
-				
-				for(int myNum : myNumbers) {
-					if(myNum == inputNum) {
+
+				for (int myNum : myNumbers) {
+					if (myNum == inputNum) {
 						isDuplOk = false;
 					}
 				}
-				
+
 				if (isRangeOk && isDuplOk) {
-					
+
 					myNumbers[i] = inputNum;
-					
+
 					break;
 				}
 
@@ -38,6 +38,30 @@ public class MainDrive {
 
 		}
 
+		int[] winNumbers = new int[6];
+
+		for (int i = 0; i < winNumbers.length; i++) {
+			
+			while(true) {
+				
+//				Math.random() => 0 <= 랜덤값*45+1 < 46
+//				cf) Random 클래스를 활용해도 됨.
+				int randomNum = (int) (Math.random()*45+1);
+				
+				
+				
+			}
+			
+		}
+
 	}
 
 }
+
+
+
+
+
+
+
+
